09cb759dca3f200a6a3edaf5fc5fddd68bbbc3e0
Gabriel-Noronha <gabrielhn999@gmail.com>
2016-03-23 22:40:12 -0300

    potenciacao

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -16,6 +16,33 @@ public class Calculadora {
 	public Integer dividir(Integer a, Integer b){
 		return a / b;
 	}
+
+
+//fazendo exponencial
+
+Scanner ent = new Scanner(System.in);
+       int base, pot;
+       int res = 1;
+              
+        do{
+            System.out.println("Digite a base");
+            base = ent.nextInt();
+            
+            if(base != 0){
+                System.out.println("Digite a potencia");
+                pot = ent.nextInt();
+        
+                do{
+                    res = res * base;
+                    pot--;    
+                }while(pot > 0);
+        
+                System.out.println(res);
+                res = 1;
+            }
+        }while((base != 0));
+        System.out.println("Você digitou base = 0. O programa foi finalizado.");
+    }
 	
 
 }
